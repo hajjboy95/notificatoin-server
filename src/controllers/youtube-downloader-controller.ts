5061f15a931ab2b4a221ab97c5873c7f0b4a9a02
Ismail El-Habbash <gizmo-boy95@hotmail.com>
2019-08-25 17:31:39 +0800

    commit

--- src/controllers/youtube-downloader-controller.ts
+++ src/controllers/youtube-downloader-controller.ts
@@ -2,41 +2,45 @@ import { Request, Response, NextFunction } from 'express'
 import * as fs from 'fs'
 var ytdl = require('ytdl-core');
 
+
 export class YoutubeDownloaderController {
-    downloadsPath =  `${__dirname}/../vids/`
+    downloadsPath = `${__dirname}/../vids/`
 
     constructor() {
         this.downloadVideo = this.downloadVideo.bind(this) //makes me bind this to proper this
         this.indexpage = this.indexpage.bind(this) //makes me bind this to proper this
     }
 
-    public downloadVideo(req: Request, res: Response, next: NextFunction) {
-        console.log("HELLO downloadVideo" )
-        console.log(req.body)
+    public async downloadVideo(req: Request, res: Response, next: NextFunction) {
         const url = req.body.ytURL
         const videoFormat = req.body.videoFormat
-        console.log(`url = ${url} \nvideoFormat = ${videoFormat}`)
-        res.render('index.ejs', {videoUrl: url, format: videoFormat })
-        // this.download(url,req,res,next)
+
+        try {
+            const vidPath = await this.download(url, videoFormat)
+            res.render('index.ejs', { videoUrl: url, format: videoFormat, err: null, vidPath: vidPath })
+        } catch (error) {
+            res.render('index.ejs', { videoUrl: url, format: videoFormat, err: error, vidPath: null })
+        }
     }
 
     public indexpage(req: Request, res: Response, next: NextFunction) {
         console.log("HELLO")
-        // const url = req.query.ytURL
-        // this.download(url,req,res,next)
-        res.render('index.ejs', {videoUrl: null, format: null })
+        res.render('index.ejs', { videoUrl: null, format: null })
     }
 
-    private download(url: String, req: Request, res: Response, next: NextFunction) {
-        const currentTime = Date.now()
-        const vidPath = `${this.downloadsPath}${currentTime}-vid.mp4`
-
-        ytdl(url, { filter: (format) => format.container === 'mp4' })
-        .pipe(fs.createWriteStream(vidPath))
-        .on('finish', () => {
-            res.download(vidPath)
-        }).on('error', (e) => {
-            next(e)
+    private async download(url: String, format: String) {
+        return new Promise((res, rej) => {
+            const currentTime = Date.now()
+            const vidPath = `${this.downloadsPath}${currentTime}-vid.mp4`
+            
+            ytdl(url, { filter: (format) => format.container === 'mp4' })
+                .pipe(fs.createWriteStream(vidPath))
+                .on('finish', () => {
+                    res(vidPath)
+                }).on('error', (e) => {
+                    rej(e)
+                })
         })
+
     }
 }
